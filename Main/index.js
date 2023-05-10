18ed984da212a85e7687ca1ce503b958a262f78c
Mobark Walid <Mobarkbacran1088@gmail.com>
2023-05-10 18:53:07 +0300

    Played around index.js and centered div on index.html

--- Main/index.js
+++ Main/index.js
@@ -1,5 +1,6 @@
 // Calling JSON files
 import Courses from "./JSON/Courses.json" assert { type: "json" };
+// import Courses from "./JSON/Courses.json" assert { type: "json" };
 
 const Grades = {
   "A+": 95,
@@ -12,29 +13,24 @@ const Grades = {
   D: 60,
 };
 
-function GetDifficulty(Course) {
-  for (let i = 0; i < Course.length; i++) {
-    console.log(Grades[Course]);
-  }
-}
 // Calculus I
-console.log(Courses.courses[0].grades.length);
-// Introduction to CS
-console.log(Courses.courses[1]);
-// History of Art
-console.log(Courses.courses[2]);
-// Data Structures
-console.log(Courses.courses[3]);
-// Algorithms
-console.log(Courses.courses[4]);
-// Programming Languages
-console.log(Courses.courses[5]);
-// Database Systems
-console.log(Courses.courses[6]);
-// Computer Networks
-console.log(Courses.courses[7]);
-// Artificial Intelligence
-console.log(Courses.courses[8]);
+console.log(Courses.courses[0].name);
+// // Introduction to CS
+// console.log(Courses.courses[1]);
+// // History of Art
+// console.log(Courses.courses[2]);
+// // Data Structures
+// console.log(Courses.courses[3]);
+// // Algorithms
+// console.log(Courses.courses[4]);
+// // Programming Languages
+// console.log(Courses.courses[5]);
+// // Database Systems
+// console.log(Courses.courses[6]);
+// // Computer Networks
+// console.log(Courses.courses[7]);
+// // Artificial Intelligence
+// console.log(Courses.courses[8]);
 
 /////////////////////////////////
 //Formula to get average grade//
@@ -47,3 +43,7 @@ for (let i = 0; i < Courses.courses[2].grades.length; i++) {
 
 let average = value / Courses.courses[0].grades.length;
 console.log(average);
+
+document.getElementById("button").addEventListener("click", () => {
+  document.getElementById("UserInfomration").innerHTML = Courses.courses[0];
+});
