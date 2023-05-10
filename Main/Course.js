9a10af17685dc40bbc8e9901dfa7e37a531c9ed8
Mobark Walid <Mobarkbacran1088@gmail.com>
2023-05-10 20:49:40 +0300

    Created a function in Course.js that lets us get the average of the student

---
+++ Main/Course.js
@@ -0,0 +1,68 @@
+// Calling JSON files
+import Courses from "./JSON/Courses.json" assert { type: "json" };
+import Students from "./JSON/Students.json" assert { type: "json" };
+
+const Grades = {
+  "A+": 95,
+  A: 90,
+  "B+": 85,
+  B: 80,
+  "C+": 75,
+  C: 70,
+  "D+": 65,
+  D: 60,
+};
+/* 
+// Calculus I
+console.log(Courses.courses[0].name);
+ // Introduction to CS
+ console.log(Courses.courses[1]);
+ // History of Art
+ console.log(Courses.courses[2]);
+ // Data Structures
+ console.log(Courses.courses[3]);
+ // Algorithms
+ console.log(Courses.courses[4]);
+ // Programming Languages
+ console.log(Courses.courses[5]);
+ // Database Systems
+ console.log(Courses.courses[6]);
+ // Computer Networks
+ console.log(Courses.courses[7]);
+ // Artificial Intelligence
+ console.log(Courses.courses[8]);
+  */
+
+/////////////////////////////////
+//Formula to get average grade//
+///////////////////////////////
+
+// let value = 0;
+// for (let i = 0; i < Courses.courses[2].grades.length; i++) {
+//   value = value + Grades[Courses.courses[2].grades[i]];
+// }
+
+// let average = value / Courses.courses[0].grades.length;
+// console.log(average);
+
+/////////////////////////////////
+
+/////////////////////////////////
+//   Function for students    //
+///////////////////////////////
+
+export function StudentAverage(ID) {
+  let Student = Students.students["201921009"];
+  let Name = Student.studentName;
+  let stdID = Student.studentID;
+
+  let value = 0;
+  for (let i = 0; i < Student.courses.length; i++) {
+    value = value + Grades[Student.courses[i].grade];
+  }
+
+  let average = value / Student.courses.length;
+  console.log(Name, average);
+}
+
+StudentAverage();
--- Main/index.js
+++ Main/index.js
@@ -1,49 +1,18 @@
-// Calling JSON files
-import Courses from "./JSON/Courses.json" assert { type: "json" };
-// import Courses from "./JSON/Courses.json" assert { type: "json" };
-
-const Grades = {
-  "A+": 95,
-  A: 90,
-  "B+": 85,
-  B: 80,
-  "C+": 75,
-  C: 70,
-  "D+": 65,
-  D: 60,
-};
-
-// Calculus I
-console.log(Courses.courses[0].name);
-// // Introduction to CS
-// console.log(Courses.courses[1]);
-// // History of Art
-// console.log(Courses.courses[2]);
-// // Data Structures
-// console.log(Courses.courses[3]);
-// // Algorithms
-// console.log(Courses.courses[4]);
-// // Programming Languages
-// console.log(Courses.courses[5]);
-// // Database Systems
-// console.log(Courses.courses[6]);
-// // Computer Networks
-// console.log(Courses.courses[7]);
-// // Artificial Intelligence
-// console.log(Courses.courses[8]);
-
 /////////////////////////////////
-//Formula to get average grade//
+//   Program Functionality    //
 ///////////////////////////////
-let value = 0;
 
-for (let i = 0; i < Courses.courses[2].grades.length; i++) {
-  value = value + Grades[Courses.courses[2].grades[i]];
+// import { StudentAverage } from "./Course";
+
+// When the user fills-up the information request
+// document.getElementById("button").addEventListener("click", () => {});
+
+// document.getElementById("myButton").addEventListener("click", getInfomration());
+function getInfo(value) {
+  let ID = document.getElementById("StudentID").value;
+  let TARGET = document.getElementById("GPA").value;
+
+  console.log("Student ID :" + ID + "  /  " + "Target GPA" + TARGET);
 }
 
-let average = value / Courses.courses[0].grades.length;
-console.log(average);
-
-document.getElementById("button").addEventListener("click", () => {
-  document.getElementById("UserInfomration").innerHTML = Courses.courses[0];
-});
+// StudentAverage();
